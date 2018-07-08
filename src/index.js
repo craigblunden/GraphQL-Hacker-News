58d064cdb7b163f7dfc8e9916bcfe42e8d27342b
craigblunden <craig@neto.com.au>
2018-07-08 23:05:39 +1000

    hackernews backend completed

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { Prisma } = require('prisma-binding')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const AuthPayload = require('./resolvers/AuthPayload')
+const Subscription = require('./resolvers/Subscription')
+const Feed = require('./resolvers/Feed')
 
-// 1
-const typeDefs = `
-type Query {
-  info: String!
-}
-`
-
-// 2
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`
-  }
+  Query,
+  Mutation,
+  AuthPayload,
+  Subscription,
+  Feed
 }
 
-// 3
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'https://us1.prisma.sh/craig-blunden-315eb8/hackernews/dev',
+      secret: 'mysecret123',
+      debug: true,
+    }),
+  }),
 })
+
 server.start(() => console.log(`Server is running on http://localhost:4000`))
