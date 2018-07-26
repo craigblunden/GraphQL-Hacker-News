a8bc6cfccc63631571f3828437c90b31ab604b5a
craigblunden <craig@neto.com.au>
2018-07-26 21:50:31 +1000

    fixed edit post page

--- src/components/EditPost.js
+++ src/components/EditPost.js
@@ -1,57 +1,63 @@
-import React, { Component } from 'react'
-import { graphql } from 'react-apollo'
-import { gql } from 'apollo-boost'
+import React, { Component } from "react"
+import { Query } from "react-apollo"
+import { gql } from "apollo-boost"
 
 class EditPost extends Component {
-  state = {
-    description: '',
-    url: '',
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="">
-          <input
-            className="mb2"
-            value={this.state.description}
-            onChange={e => this.setState({ description: e.target.value })}
-            type="text"
-            placeholder="A description for the link"
-          />
-          <input
-            className="mb2"
-            value={this.state.url}
-            onChange={e => this.setState({ url: e.target.value })}
-            type="text"
-            placeholder="The URL for the link"
-          />
-        </div>
-        <button onClick={() => this._EditPost()}>Submit</button>
-      </div>
+  render(){
+      const id = this.props.match.params.id;
+      return (
+        <Query query={POST_QUERY} variables={{id}}>
+          {({ loading, error, data }) => {
+            if (loading) return "Loading...";
+            if (error) return `Error! ${error.message}`;
+            console.log(this)
+            return (
+              <div>
+                <div className="">
+                  <input
+                    className="mb2"
+                    value={data.link.description}
+                    onChange={e => this.setState({ description: e.target.value })}
+                    type="text"
+                    placeholder="A description for the link"
+                  />
+                  <input
+                    className="mb2"
+                    value={data.link.url}
+                    onChange={e => this.setState({ url: e.target.value })}
+                    type="text"
+                    placeholder="The URL for the link"
+                  />
+                </div>
+                <button onClick={() => this._EditPost()}>Submit</button>
+              </div>
+            )
+          }
+        }
+      </Query>
     )
   }
-
-  _EditPost = async () => {
-    const { description, url } = this.state
-    await this.props.postQuery({
-      variables: {
-        description,
-        url,
-      },
-    })
-  }
-
 }
 
 const POST_QUERY = gql`
-  query postQuery($ID: ID!) {
-    link(id: "cjjwqx70bzmek0b022dwd8qf1") {
-      id
-      url
-      description
-    }
-  }
+  query link($id: ID!) {
+  link(where:{
+    id: $id}
+  ){
+    id,
+    description,
+    url
+  } 
+}
 `
 
-export default graphql(POST_QUERY, { name: 'postQuery' })(EditPost)
+const _EditPost = async () => {
+  const { id, description, url } = this.state
+  await this.props.postQuery({
+    variables: {
+      id: this.match.params.id
+    },
+  })
+}
+
+export default EditPost
