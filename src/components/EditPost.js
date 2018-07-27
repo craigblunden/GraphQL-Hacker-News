e455a97e149d14d2a0322aa7dc2f9960d5bc2b62
craigblunden <craig@neto.com.au>
2018-07-27 20:03:09 +1000

    basic editPost styling

--- src/components/EditPost.js
+++ src/components/EditPost.js
@@ -3,39 +3,66 @@ import { Query } from "react-apollo"
 import { gql } from "apollo-boost"
 
 class EditPost extends Component {
+  
+  state = {
+    description: '',
+    url: ''
+  }
+
   render(){
+
       const id = this.props.match.params.id;
+
       return (
         <Query query={POST_QUERY} variables={{id}}>
           {({ loading, error, data }) => {
             if (loading) return "Loading...";
             if (error) return `Error! ${error.message}`;
-            console.log(this)
+            console.log(this.state)
             return (
-              <div>
-                <div className="">
-                  <input
-                    className="mb2"
-                    value={data.link.description}
-                    onChange={e => this.setState({ description: e.target.value })}
-                    type="text"
-                    placeholder="A description for the link"
-                  />
-                  <input
-                    className="mb2"
-                    value={data.link.url}
-                    onChange={e => this.setState({ url: e.target.value })}
-                    type="text"
-                    placeholder="The URL for the link"
-                  />
+              <div className="container">
+                <div className="row">
+                  <form> 
+                    <div className="form-group">
+                      <label htmlFor="editDescription">Description</label>
+                      <input
+                        id="editDescription"
+                        className="form-control"
+                        value={this.state.description || data.link.description}
+                        onChange={e => this.setState({ description: e.target.value })}
+                        type="text"
+                        placeholder="A description for the link"
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="editLink">Link</label>
+                      <input
+                        id="editLink"
+                        className="form-control"
+                        value={this.state.url || data.link.url}
+                        onChange={e => this.setState({ url: e.target.value })}
+                        type="text"
+                        placeholder="The URL for the link"
+                      />
+                    </div>
+                    <button type="submit" onSubmit={() => this._EditPost()}>Submit</button>
+                  </form>
                 </div>
-                <button onClick={() => this._EditPost()}>Submit</button>
               </div>
             )
           }
         }
       </Query>
     )
+  }
+  _EditPost = async (e) => {
+    e.preventDefault();
+    const { id, description, url } = this.state
+    await this.props.postQuery({
+      variables: {
+        id: this.match.params.id
+      },
+    })
   }
 }
 
@@ -51,13 +78,6 @@ const POST_QUERY = gql`
 }
 `
 
-const _EditPost = async () => {
-  const { id, description, url } = this.state
-  await this.props.postQuery({
-    variables: {
-      id: this.match.params.id
-    },
-  })
-}
+
 
 export default EditPost
